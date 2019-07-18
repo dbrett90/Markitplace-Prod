6298ace8671c2e75776517c56154dd2f443e8352
dbrett90 <brettd@bc.edu>
2019-07-18 19:10:55 -0400

    Finish User Signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/our-team', to: 'static_pages#our_team'
   get '/contact', to: 'static_pages#contact'
-
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
