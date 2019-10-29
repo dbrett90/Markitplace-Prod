5b0e0dc7bfe21ee7815b6f09cd6e5cef97317ecf
dbrett90 <brettd@bc.edu>
2019-10-29 16:49:05 -0400

    Updated Gems

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,12 +54,11 @@ class UsersController < ApplicationController
   def business_or_customer_create
     select_output = params[:business_or_customer][:business_or_customer_select]
     Stripe.api_key = Rails.application.credentials.stripe_secret_key
-    #For testing purposes
-    flash[:success] = Stripe.api_key
     #Make sure the "Purchase Meal Kits" is not changed in the view as it will
     #affect the output of this controller. Binary Value
     if select_output == "Purchase Meal Kits"
       redirect_to signup_url
+      flash[:success] = Stripe.api_key
     else
       redirect_to "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_FX0EKPNDzWlcxcjjUNnxNAhUa0cjuVBI&scope=read_write"
       #Attempting to retrieve customer info from controller
