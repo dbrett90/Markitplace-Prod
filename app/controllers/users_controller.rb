a71d331aa0d54d7517b9790129b93e91cd052077
dbrett90 <brettd@bc.edu>
2019-11-05 13:16:54 -0500

    Trying connection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,16 +67,14 @@ class UsersController < ApplicationController
   def grab_stripe_details
     Stripe.api_key = Rails.application.credentials.development[:stripe_api_key]
     auth_code = params[:code]
-    flash[:success] = Stripe.api_key
+    response = Stripe::OAuth.token({
+      grant_type: 'authorization_code',
+      code: auth_code
+    })
+    connected_account_id = response.stripe_user_id
+    flash[:success] = connected_account_id
+    flash[:danger] = auth_code
     render 'static_pages/home'
-    # auth_code = params[:code]
-    # response = Stripe::OAuth.token({
-    #   grant_type: 'authorization_code',
-    #   code: auth_code
-    # })
-    # connected_account_id = response.stripe_user_id
-    # flash[:success] = connected_account_id
-    # flash[:danger] = auth_code
   end
 
   private
