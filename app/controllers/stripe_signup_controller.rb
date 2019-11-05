c072610474bf4eeed0ae2dc5f583b45944a33633
dbrett90 <brettd@bc.edu>
2019-11-05 15:24:00 -0500

    Updated Routes and Controller

--- app/controllers/stripe_signup_controller.rb
+++ app/controllers/stripe_signup_controller.rb
@@ -1,5 +1,17 @@
 class StripeSignupController < ApplicationController
 
-    def stripe_signup
+    def new
+        Stripe.api_key = Rails.application.credentials.development[:stripe_api_key]
+        @user = User.new()
+        auth_code = params[:code]
+        response = Stripe::OAuth.token({
+          grant_type: 'authorization_code',
+          code: auth_code
+        })
+        connected_account_id = response.stripe_user_id
+        #Check that you have the connected account ID
+        flash[:success] = connected_account_id
+        flash[:danger] = auth_code
+        render 'static_pages/home' 
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,18 +64,6 @@ class UsersController < ApplicationController
     end
   end
 
-  def grab_stripe_details
-    Stripe.api_key = Rails.application.credentials.development[:stripe_api_key]
-    auth_code = params[:code]
-    response = Stripe::OAuth.token({
-      grant_type: 'authorization_code',
-      code: auth_code
-    })
-    connected_account_id = response.stripe_user_id
-    flash[:success] = connected_account_id
-    flash[:danger] = auth_code
-    render 'static_pages/home'
-  end
 
   private
 
