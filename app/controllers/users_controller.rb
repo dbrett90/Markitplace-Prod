06067ae2fda7ce45731467856f3fb33eca011a93
dbrett90 <brettd@bc.edu>
2019-11-05 13:03:51 -0500

    Fixed wrong route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,15 +66,15 @@ class UsersController < ApplicationController
 
   def grab_stripe_details
     render 'static_pages/home'
-    Stripe.api_key = Rails.application.credentials.development[:stripe_api_key]
-    auth_code = params[:code]
-    response = Stripe::OAuth.token({
-      grant_type: 'authorization_code',
-      code: auth_code
-    })
-    connected_account_id = response.stripe_user_id
-    flash[:success] = connected_account_id
-    flash[:danger] = auth_code
+    # Stripe.api_key = Rails.application.credentials.development[:stripe_api_key]
+    # auth_code = params[:code]
+    # response = Stripe::OAuth.token({
+    #   grant_type: 'authorization_code',
+    #   code: auth_code
+    # })
+    # connected_account_id = response.stripe_user_id
+    # flash[:success] = connected_account_id
+    # flash[:danger] = auth_code
   end
 
   private
