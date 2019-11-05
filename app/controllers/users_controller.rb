d0835b4cadbdc65fbcd28c28711eea97256c9142
dbrett90 <brettd@bc.edu>
2019-11-05 10:25:11 -0500

    Overloaded it with info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,7 +67,7 @@ class UsersController < ApplicationController
       #stripe_callback(Stripe.api_key)
     end
     #orignial_url will just give you the base url
-    flash[:success] = request.headers
+    flash[:success] = request.headers.last
   end
 
   private
