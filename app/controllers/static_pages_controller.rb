238fdd1f6b5a1c57dae911386bcc466fb1dd48b2
dbrett90 <brettd@bc.edu>
2019-11-05 12:29:30 -0500

    Update static_users controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    flash[:success] = params
   end
 
   def our_team
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,9 +67,6 @@ class UsersController < ApplicationController
       #stripe_callback(Stripe.api_key)
     end
     #orignial_url will just give you the base url
-    flash[:danger] = request.query_string
-    flash[:success] = request.url
-    flash[:notice] = request.post?
   end
 
   private
