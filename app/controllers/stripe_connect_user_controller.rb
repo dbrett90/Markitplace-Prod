20769740abf6356ce99c1e489976e9f31ff14e82
dbrett90 <brettd@bc.edu>
2019-11-14 10:22:49 -0500

    Updating stripe user model

--- app/controllers/stripe_connect_user_controller.rb
+++ app/controllers/stripe_connect_user_controller.rb
@@ -7,12 +7,14 @@ class StripeConnectUserController < ApplicationController
           grant_type: 'authorization_code',
           code: auth_code
         })
+        dummy_email = "danbrett107@gmail.com"
         connected_account_id = response.stripe_user_id
-        @stripe_connect_user = StripeConnectUser.new(connected_account_id)
+        @stripe_connect_user = StripeConnectUser.new(connected_account_id, dummy_email)
         @stripe_connect_user.save
         #Check that you have the connected account ID
-        # flash[:success] = connected_account_id
-        # flash[:danger] = auth_code
+        flash[:success] = connected_account_id
+        flash[:danger] = auth_code
+        flash[:notice] = "User has been saved successfully"
         render 'static_pages/home' 
     end
 end
--- app/models/stripe_connect_user.rb
+++ app/models/stripe_connect_user.rb
@@ -1,3 +1,5 @@
 class StripeConnectUser < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :stripe_id, presence: true
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
 end
