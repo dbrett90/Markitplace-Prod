027e5f744e7b84c09679ca37dd9c1501a94ea804
dbrett90 <brettd@bc.edu>
2019-11-16 12:46:59 -0500

    Testing with flash notice

--- app/controllers/stripe_connect_user_controller.rb
+++ app/controllers/stripe_connect_user_controller.rb
@@ -10,10 +10,10 @@ class StripeConnectUserController < ApplicationController
         dummy_email = "danbrett107@gmail.com"
         connected_account_id = response.stripe_user_id
         @stripe_connect_user = StripeConnectUser.new
+        flash[:danger] = @stripe_connect_user.class
         @stripe_connect_user.stripe_id = connected_account_id
         @stripe_connect_user.stripe_email = dummy_email 
-        @stripe_connect_user.save
-        # @stripe_connect_user.save
+        #@stripe_connect_user.save
         #Check that you have the connected account ID
         #flash[:success] = connected_account_id
         #flash[:danger] = auth_code
