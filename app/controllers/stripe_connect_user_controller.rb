3d2607803e82d9cdd5b3f8a9f1da32ac64ea26f6
dbrett90 <brettd@bc.edu>
2019-11-16 13:13:26 -0500

    Pushed notice to production

--- app/controllers/stripe_connect_user_controller.rb
+++ app/controllers/stripe_connect_user_controller.rb
@@ -14,7 +14,7 @@ class StripeConnectUserController < ApplicationController
         @stripe_connect_user.stripe_email = dummy_email 
         @stripe_connect_user.save
         #Check that you have the connected account ID
-        flash[:success] = connected_account_id
+        flash[:notice] = connected_account_id
         flash[:danger] = auth_code
         flash[:success] = "Your stripe account has now been linked!"
         render 'static_pages/home' 
