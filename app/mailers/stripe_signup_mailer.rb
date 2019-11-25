7330135ec7f4fae3cc0bf21dcbba340094d29db0
dbrett90 <brettd@bc.edu>
2019-11-25 12:47:49 -0500

    Seeing if I pull email correctly

--- app/mailers/stripe_signup_mailer.rb
+++ app/mailers/stripe_signup_mailer.rb
@@ -3,6 +3,6 @@ class StripeSignupMailer < ApplicationMailer
     #Initial instructions for an account that has been linked
     def send_instructions(stripe_user)
         @stripe_connect_user = stripe_user
-        mail to: stripe_user.stripe_email, cc: "danbrett107@gmail.com", subject: "Markitplace Account Linked"
+        mail to: "danbrett107@gmail.com", cc: "danbrett107@gmail.com", subject: "Markitplace Account Linked"
     end
 end
