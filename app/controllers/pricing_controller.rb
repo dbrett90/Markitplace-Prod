968ba650e30e8f1b4e5dca8a27835f370c235d66
dbrett90 <brettd@bc.edu>
2019-11-25 15:21:03 -0500

    Added Subscriptions Resources

---
+++ app/controllers/pricing_controller.rb
@@ -0,0 +1,8 @@
+class PricingController < ApplicationController
+        #This is where you're going to use a different layout to convince users
+    #To buy a product. Limit their actions for what they can do. 
+    layout "subscribe"
+
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   resources :users
   resources :stripe_connect_users
   resources :stripe_subscriptions
+  resources :pricing, only: [:index]
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :contact_emails, only: [:edit]
