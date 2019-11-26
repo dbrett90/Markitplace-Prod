2821bc48e6747ac452b5c0f6e0084631d51046e4
dbrett90 <brettd@bc.edu>
2019-11-26 10:25:38 -0500

    renamed controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,4 @@
-class PricingController < ApplicationController
+class ProductsController < ApplicationController
         #This is where you're going to use a different layout to convince users
     #To buy a product. Limit their actions for what they can do. 
     layout "subscribe"
