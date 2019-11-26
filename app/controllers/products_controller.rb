ddf908e5325d6706456c6e5b4655923a7f0c8aff
dbrett90 <brettd@bc.edu>
2019-11-26 10:37:57 -0500

    Testing jumbotron

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
         #This is where you're going to use a different layout to convince users
     #To buy a product. Limit their actions for what they can do. 
-    layout "subscribe"
+    # layout "subscribe"
 
     def index
     end
