63a60286a766e3d0a5ba997f2c153cb2e7767d71
dbrett90 <brettd@bc.edu>
2020-01-08 15:56:35 -0500

    Updated jbuilder

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create!(name:  "Daniel Brett",
-#              email: "dbrett14@gmail.com",
-#              password:              "Hello#123!",
-#              password_confirmation: "Hello#123!",
-#              admin: true, 
-#              activated: true,
-#              activated_at: Time.zone.now)
+User.create!(name:  "Daniel Brett",
+             email: "dbrett14@gmail.com",
+             password:              "Hello#123!",
+             password_confirmation: "Hello#123!",
+             admin: true, 
+             activated: true,
+             activated_at: Time.zone.now)
 
 
 
