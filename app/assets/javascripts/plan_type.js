1b534f4862402c8288f0c0f1124be728f5162828
dbrett90 <brettd@bc.edu>
2020-01-16 18:10:52 -0500

    Updated the values

--- app/assets/javascripts/plan_type.js
+++ app/assets/javascripts/plan_type.js
@@ -1,5 +1,6 @@
-$(".fa-chevron-circle-down").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#plan-offerings").offset().top},
-        'slow');
+$('.fa-chevron-circle-down').click(function() {
+    var sectionTo = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(sectionTo).offset().top
+    }, 1500);
 });
