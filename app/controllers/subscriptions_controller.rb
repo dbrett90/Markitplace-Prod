41d78fe78002a219ad0266b6eb4a6f6bad637448
dbrett90 <brettd@bc.edu>
2020-01-25 12:59:50 -0500

    Updated ActionMailer on subscriptions controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -37,7 +37,14 @@ class SubscriptionsController < ApplicationController
             card_type: params[:user][:card_type]
             ) if params[:user][:card_last4]
             current_user.update(options)
-            redirect_to root_path, notice: "Your subscription is now active!"
+            redirect_to root_path
+
+            #Trigger Flash & The action mailers for confirmation
+            flash[:success] = "Your subscription is now active! Please check your email for a confirmation notice."
+            OrderConfirmationMailer.customer_confirmation(params[:payment_shipping][:plan], 
+            params[:payment_shipping][:recipient_name], params[:payment_shipping][:street_address_1],
+            params[:payment_shipping][:street_address_2], params[:payment_shipping][:city],
+            params[:payment_shipping][:state], params[:payment_shipping][:zipcode])
     end
 
     def destroy
