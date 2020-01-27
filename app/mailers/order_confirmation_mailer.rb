2195e6dd1dd9e771503fe8765de2dfe3a8f8bb1e
dbrett90 <brettd@bc.edu>
2020-01-27 15:15:13 -0500

    Forgot to update the class name in mailer

--- app/mailers/order_confirmation_mailer.rb
+++ app/mailers/order_confirmation_mailer.rb
@@ -1,22 +1,25 @@
 
-def customer_confirmation(plan_type, recipient_name, street_address_1, street_address_2, city, state, zipcode)
-    @user_name = current_user.name
-    @user_email = current_user.email
-    @plan_type = plan_type 
-    @recipient_name = recipient_name
-    @street_address_1 = street_address_1
-    @street_address_2 = street_address_2
-    @city = city
-    @state = state
-    @zipcode = zipcode
-    #Remove the CC afterwards.... just for testing purposes. 
-    mail to: "danbrett107@gmail.com", cc: "dbrett14@gmail.com", subject: "Markitplace Subscription Confirmation"
-end
+class OrderConfirmationMailer < ApplicationMailer
+    
+    def customer_confirmation(plan_type, recipient_name, street_address_1, street_address_2, city, state, zipcode)
+        @user_name = current_user.name
+        @user_email = current_user.email
+        @plan_type = plan_type 
+        @recipient_name = recipient_name
+        @street_address_1 = street_address_1
+        @street_address_2 = street_address_2
+        @city = city
+        @state = state
+        @zipcode = zipcode
+        #Remove the CC afterwards.... just for testing purposes. 
+        mail to: "danbrett107@gmail.com", cc: "dbrett14@gmail.com", subject: "Markitplace Subscription Confirmation"
+    end
 
-def vendor_confirmation(customer_name, customer_email, vendor_email, plan_type, shipping_details)
-    @customer_name = customer_name
-    @customer_email = customer_email
-    @plan_type = plan_type
-    @shipping_details = shipping_details
-    mail to: vendor_email, cc: "dbrett14@gmail.com", subject: "customer has placed an order on Markitplace"
+    def vendor_confirmation(customer_name, customer_email, vendor_email, plan_type, shipping_details)
+        @customer_name = customer_name
+        @customer_email = customer_email
+        @plan_type = plan_type
+        @shipping_details = shipping_details
+        mail to: vendor_email, cc: "dbrett14@gmail.com", subject: "customer has placed an order on Markitplace"
+    end
 end
