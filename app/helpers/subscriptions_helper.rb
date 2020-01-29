75cfbe0e2564b131a9fbd264bcfa2dbfc81a4960
dbrett90 <brettd@bc.edu>
2020-01-29 13:14:52 -0500

    Updated the helper function

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,7 +1,9 @@
-def find_plan(stripe_subscription, subscription_plans)
-    subscription_plans.each do |plan_type|
-        if stripe_subscription.nickname.downcase == plan_type.name.downcase
-            return plan_type
+module SubscriptionsHelper
+    def find_plan(stripe_subscription, subscription_plans)
+        subscription_plans.each do |plan_type|
+            if stripe_subscription.nickname.downcase == plan_type.name.downcase
+                return plan_type
+            end
         end
     end
 end
