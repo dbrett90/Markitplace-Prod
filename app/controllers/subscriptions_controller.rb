fbd85043bcaf505ed1b826ba9600409bf02620ac
dbrett90 <brettd@bc.edu>
2020-01-29 13:31:16 -0500

    Updated the helper function

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -88,4 +88,14 @@ class SubscriptionsController < ApplicationController
 
         redirect_to subscription_library_index_path, notice: "Your subscription has been cancelled"
     end
+
+    private
+
+    def find_plan(stripe_subscription, subscription_plans)
+        subscription_plans.each do |plan_type|
+            if stripe_subscription.nickname.downcase == plan_type.name.downcase
+                return plan_type
+            end
+        end
+    end
 end
