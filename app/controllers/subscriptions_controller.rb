80f955985b36217b90a0445e3f9cc7b403d38391
dbrett90 <brettd@bc.edu>
2020-02-05 21:52:36 -0500

    Updated create with a hash

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -86,7 +86,8 @@ class SubscriptionsController < ApplicationController
         plan_type_downcased = plan_type.name.downcase
         flash[:danger] = plan_type_downcased
         #Think we're going to need to update the datavase with a has of plan_name:subscription_id
-        subscription = customer.subscriptions.retrieve(nickname: plan_type_downcased)
+        subscription_id = customer.stripe_subscription_id[plan_type_downcased]
+        subscription = customer.subscriptions.retrieve(id: subscription_id)
         flash[:warning] = subscription
 
         #Remove from the user's library additions
