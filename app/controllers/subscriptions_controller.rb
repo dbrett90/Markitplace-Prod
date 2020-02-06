0f6b77f4fdfd9b0155f034caff0fca0320ac245b
dbrett90 <brettd@bc.edu>
2020-02-06 11:24:05 -0500

    Updating the buttons and comments

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -36,12 +36,8 @@ class SubscriptionsController < ApplicationController
         current_user.stripe_subscription_id[plan.nickname.downcase] = subscription.id
         options = {
             stripe_id: customer.id,
-            #Gonna need to check this line here
-            # stripe_subscription_id << subscription.id,
             subscribed: true
         }
-        # flash[:danger] = subscription.class
-        # flash[:success] = subscription
 
         # #Doing a merge if card value is updated. Below function will check this
         options.merge!(
@@ -52,17 +48,21 @@ class SubscriptionsController < ApplicationController
             ) if params[:user][:card_last4]
             current_user.update(options)
         
-        #Let's add subscription value to the Library... will need to make sure dependencies operating correctly.
+        #Let's send the payment over via connect
+        
+        #Let's add subscription value to the Library.
         subscription_plans = PlanType.all
-        #Took the function out and put it in it's own function in private
+
+        #calling private function find_plan
         plan_type = find_plan(plan, subscription_plans)
         current_user.plan_subscription_library_additions << plan_type
-        # flash[:warning] = params
+
         #Trigger Flash & The action mailers for confirmation
         OrderConfirmationMailer.customer_confirmation(current_user, plan.nickname, 
             params[:payment_shipping][:recipient_name], params[:payment_shipping][:street_address_1],
             params[:payment_shipping][:street_address_2], params[:payment_shipping][:city],
             params[:payment_shipping][:state], params[:payment_shipping][:zipcode]).deliver_now
+
         #Hit the order confirmation and send over to the vendor... need to pull the vendor email
         #from Stripe, but question becomes how to test for that.
         #OrderConfirmationMailer.vendor_confirmation(current_user)
@@ -70,38 +70,34 @@ class SubscriptionsController < ApplicationController
         #Redirect back to the root Path and send flash notice
         redirect_to root_path
         flash[:success] = "Your subscription is now active! Please check your email for a confirmation notice."
-        # flash[:warning] = params[:plan]
-        # flash[:notice] = params
 
     end
 
-    #We have some work here - not properly deleting as of now
+    #Method to unsubscribe from a current subscription in Stripe
     def destroy
         Stripe.api_key = Rails.application.credentials.development[:stripe_api_key]
         customer = Stripe::Customer.retrieve(current_user.stripe_id)
-        #Find the current subscription that we're going to delete. Iterate over subscription ids until nickname matches up?
-        # while 
+
         #Are we pulling the ID from the params section - doesn't grab anything currently
         plan_type = PlanType.find(params[:id])
         plan_type_downcased = plan_type.name.downcase
-        # flash[:danger] = plan_type_downcased
-        #Think we're going to need to update the datavase with a has of plan_name:subscription_id
+
+        #Add the plan name and subscription ID as a hash to the subscription id table
         subscription_id = current_user.stripe_subscription_id[plan_type_downcased]
         subscription = customer.subscriptions.retrieve(id: subscription_id)
-        # flash[:warning] = subscription
 
-        #Remove from the user's library additions. May need to refactor to be more efficient later on
+        #Remove from the user's library additions. May need to refactor to be more efficient later on...
         subscription_plans = PlanType.all
+
         # Call function to find removed plan
         removed_plan = remove_plan(subscription_plans, plan_type_downcased)
         current_user.plan_subscription_library_additions.delete(removed_plan)
 
-        # #Delete the subscription from stripe and from the user... re-examine this first line
+        # #Delete the subscription from stripe and from the user...
         customer.subscriptions.retrieve(subscription.id).delete
-        #flash[:success] = current_user.stripe_subscription_id.length
         current_user.stripe_subscription_id.delete(plan_type_downcased)
-        #flash[:danger] = current_user.stripe_subscription_id.length
         current_user.subscribed = still_subscribed?(current_user)
+        #make sure to save the user to the database
         current_user.save
 
         redirect_to plan_subscription_library_index_path, notice: "Your subscription has been cancelled"
@@ -109,6 +105,7 @@ class SubscriptionsController < ApplicationController
 
     private
 
+    #For create controller method
     def find_plan(stripe_subscription, subscription_plans)
         subscription_plans.each do |plan_type|
             if stripe_subscription.nickname.downcase == plan_type.name.downcase
@@ -117,6 +114,7 @@ class SubscriptionsController < ApplicationController
         end
     end
 
+    #For delete controller method
     def remove_plan(plans, plan)
         plans.each do |each_plan|
             if each_plan.name.downcase == plan
@@ -125,6 +123,7 @@ class SubscriptionsController < ApplicationController
         end
     end
 
+    #Is the user still subscribed?
     def still_subscribed?(user)
         if user.stripe_subscription_id.length >=1
             true
