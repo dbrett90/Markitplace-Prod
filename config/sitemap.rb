ae99958c6f5229ec0dadf39cd2d01ccaded1db5e
dbrett90 <brettd@bc.edu>
2020-03-10 15:47:36 -0400

    Updated the sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,25 +2,27 @@
 SitemapGenerator::Sitemap.default_host = "https://www.markitplace.io" # Your Domain Name
 SitemapGenerator::Sitemap.public_path = 'tmp/sitemap'
 # Where you want your sitemap.xml.gz file to be uploaded.
-SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new( 
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(
+    'markitplace-sitemaps', 
     aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
     aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-    fog_provider: 'AWS',
-    fog_directory: 'markitplace-sitemaps',
-    fog_region: 'us-east-2'
+    aws_region: 'us-east-2'
+    # fog_provider: 'AWS',
+    # fog_directory: 'markitplace-sitemaps',
+    # fog_region: 'us-east-2'
 )
 
 # The full path to your bucket
 SitemapGenerator::Sitemap.sitemaps_host = "https://markitplace-sitemaps.s3.amazonaws.com"
 # The paths that need to be included into the sitemap.
 SitemapGenerator::Sitemap.create do
-    add root_path
+    add root_path, changefreq: 'daily'
+    add contact_path, changefreq: 'daily'
+    add partner_information_path, changefreq: 'daily'
     PlanType.find_each do |plan_type|
-     add plan_type_path(plan_type, locale: :en)
-     add plan_type_path(plan_type, locale: :nl)
+     add plan_type_path(plan_type), changefreq: 'daily'
     end
     Product.find_each do |product|
-     add product_path(product, locale: :en)
-     add product_path(product, locale: :nl)
+     add product_path(product), changefreq: 'daily'
     end
 end
