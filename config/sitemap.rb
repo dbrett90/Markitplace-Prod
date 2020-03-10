9a58b2ff0efcd0937f3524bd2fc1e45f0224a8a9
dbrett90 <brettd@bc.edu>
2020-03-10 15:36:10 -0400

    UPdated config

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -6,7 +6,7 @@ SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(
     aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
     aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
     fog_provider: 'AWS',
-    fog_directory: 'markitplace-product-images',
+    fog_directory: 'markitplace-sitemaps',
     fog_region: 'us-east-2'
 )
 
