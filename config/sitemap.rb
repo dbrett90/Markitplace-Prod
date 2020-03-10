e6cf1486d8506b8c39b9b76a1e0e4708496aa569
dbrett90 <brettd@bc.edu>
2020-03-10 16:25:34 -0400

    Redoing sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,3 +1,22 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://www.markitplace.io"
+# pick a place safe to write the files
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+# store on S3 using Fog (pass in configuration values as shown above if needed)
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(
+    fog_provider: 'AWS',
+    aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+    aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+    fog_directory: 'markitplace-sitemaps',
+    fog_region: 'us-east-2')
+# inform the map cross-linking where to find the other maps
+SitemapGenerator::Sitemap.sitemaps_host = "http://markitplace-sitemaps.s3.amazonaws.com/"
+# pick a namespace within your bucket to organize your maps
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
+
+
+
+
 # config/sitemap.rb
 SitemapGenerator::Sitemap.default_host = "https://www.markitplace.io" # Your Domain Name
 SitemapGenerator::Sitemap.public_path = 'tmp/sitemap'
