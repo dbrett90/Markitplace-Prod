8b6e9f45117baf2bba7f7acb08d3c028395898f5
dbrett90 <brettd@bc.edu>
2020-04-07 21:48:32 -0400

    Adding one off products

---
+++ app/models/one_off_product.rb
@@ -0,0 +1,14 @@
+class OneOffProduct < ApplicationRecord
+    #May need to change this depending on when we add the ID
+    #Uncomment this when we have stripe mechanism down
+    # validates :stripe_id, presence: true
+    has_one_attached :thumbnail
+    belongs_to :user
+    #Again what you're doing here is linking each product to a user 
+    #via the subscription libraries. Can think of it as a massive join. 
+    #may need to rethink the relationship between product and subscription libraries
+    #Need to think about thow this linkage is going to work... want the libraries to include subscriptions?
+    #Might want to have something that shows past orders?
+    # has_many :product_subscription_libraries
+    # has_many :added_products, through: :product_subscription_libraries, source: :user
+end
