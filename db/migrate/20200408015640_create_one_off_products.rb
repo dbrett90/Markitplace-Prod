af5633849c79652ce93e0e7b128b658b6a6657f0
dbrett90 <brettd@bc.edu>
2020-04-07 22:04:56 -0400

    Updated tables

--- db/migrate/20200408015640_create_one_off_products.rb
+++ db/migrate/20200408015640_create_one_off_products.rb
@@ -1,10 +1,10 @@
 class CreateOneOffProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :one_off_products do |t|
-      t.string, :name
-      t.text, :description
-      t.float, :price
-      t.string, :partner_name
+      t.string :name
+      t.text :description
+      t.float :price
+      t.string :partner_name
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_011126) do
+ActiveRecord::Schema.define(version: 2020_04_08_015640) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2020_03_24_011126) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "one_off_products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
+    t.string "partner_name"
   end
 
   create_table "plan_subscription_libraries", force: :cascade do |t|
