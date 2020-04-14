48f58f0b9e84a99e5563e4a6aefd5ff254e9eab5
dbrett90 <brettd@bc.edu>
2020-04-13 22:30:32 -0400

    Updated Zips

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -188,6 +188,7 @@ class SubscriptionsController < ApplicationController
             shipping_address = ZipCodes.identify(zipcode)
             shipping_city = shipping_address[:city]
             shipping_city = shipping_city.downcase
+            flash[:success] = shipping_city
             flash[:warning] = zipcode_list
             # flash[:warning] = shipping_city
             # shipping_address_down = shipping_address.downcase
