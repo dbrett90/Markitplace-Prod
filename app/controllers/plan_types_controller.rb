cd23bff48f9ddbf5e9404c8a1cd1199a8544fa14
dbrett90 <brettd@bc.edu>
2020-04-15 22:20:27 -0400

    remove ?

--- app/controllers/plan_types_controller.rb
+++ app/controllers/plan_types_controller.rb
@@ -1,7 +1,7 @@
 class PlanTypesController < ApplicationController
     #Updated to allow users to access a library
  before_action :set_plan_type, only: [:edit, :show, :update, :destroy, :library]
- before_action :admin_user?, except: [:index, :show]
+ before_action :admin_user, except: [:index, :show]
 #  before_action :authenticate_user!, except: [:index, :show]
 #This is what the Devise action basically does.
 #before_action :logged_in_user, except: [:index, :show]
