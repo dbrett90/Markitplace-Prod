3df594f99040e56d2ca989d60ce40a4a73bb049a
dbrett90 <brettd@bc.edu>
2020-05-30 19:16:56 -0400

    Updated the one off

--- app/models/one_off_product.rb
+++ app/models/one_off_product.rb
@@ -4,6 +4,7 @@ class OneOffProduct < ApplicationRecord
     # validates :stripe_id, presence: true
     has_one_attached :thumbnail
     #belongs_to :user, through: :cart
+    has_many :users, through: carts
     has_many :carts
     #Again what you're doing here is linking each product to a user 
     #via the subscription libraries. Can think of it as a massive join. 
