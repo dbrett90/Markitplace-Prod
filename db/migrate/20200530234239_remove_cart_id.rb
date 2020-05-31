60f075dc105e1c7b021933f8be373e31e7c92053
dbrett90 <brettd@bc.edu>
2020-05-30 20:03:45 -0400

    Updated the migrations

--- db/migrate/20200530234239_remove_cart_id.rb
+++ db/migrate/20200530234239_remove_cart_id.rb
@@ -1,5 +1,5 @@
 class RemoveCartId < ActiveRecord::Migration[5.2]
   def change
-    remove_column :one_off_products, :cart_id
+    add_column :one_off_products, :cart_id, :integer
   end
 end
--- db/migrate/20200530235631_add_cart_id_to_one_off_products_again.rb
+++ db/migrate/20200530235631_add_cart_id_to_one_off_products_again.rb
@@ -1,5 +1,4 @@
 class AddCartIdToOneOffProductsAgain < ActiveRecord::Migration[5.2]
   def change
-    add_column :one_off_products, :cart_id, :integer
   end
 end
