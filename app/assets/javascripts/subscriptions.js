283ae8b83d04cdefa0a5f0cc190825fc2f537b66
dbrett90 <brettd@bc.edu>
2020-06-10 14:11:11 -0400

    UPdate the js

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -37,7 +37,7 @@ document.addEventListener("turbolinks:load", function() {
     //       card.update({style: {base: {fontSize: '16px'}}});
     //     }
     //   });
-    $(document).on('turbolinks:load', function(){
+    $(document).load(function(){
         if (screen.width <= 667) {
             card.update({style: {base: {fontSize: '40px'}}});
         } else {
