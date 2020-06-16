b3beee8346336af79824b008430caaca7cf0dd8a
dbrett90 <brettd@bc.edu>
2020-06-16 14:09:28 -0400

    UPdated the site

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -8,6 +8,7 @@ class PartnersController < ApplicationController
         name_downcase = params[:name].downcase
         # flash[:success] = name_downcase
         @name = params[:name].gsub!('-', " ")
+        flash[Lsuccess] = @name
         @one_off_products = find_one_off_by_name(name_downcase)
         #flash[:danger] = @one_off.price
         # flash[:warning] = @one_offs.length
@@ -15,12 +16,12 @@ class PartnersController < ApplicationController
 
     private
     def find_one_off_by_name(item)
-        item.gsub!('-', " ")
+        item = item.gsub!('-', " ")
         OneOffProduct.where("partner_name ILIKE ?", item)
     end
 
     def find_plan_type_by_name(item)
-        item.gsub!('-', " ")
+        item = item.gsub!('-', " ")
         PlanType.where("partner_name ILIKE ?", item)
     end
 end
