b9bf0082c02c1d618d539bd9ec4cad0dcea32ca5
dbrett90 <brettd@bc.edu>
2020-06-16 12:30:18 -0400

    Updated the show

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -5,6 +5,16 @@ class PartnersController < ApplicationController
     end
 
     def show
-        flash[:warning] = params
+        name_downcase = params[:name].downcase
+        @one_off = find_one_off_by_name(name_downcase)
+    end
+
+    private
+    def find_one_off_by_name(item)
+        OneOffProduct.where(:name.downcase => item)
+    end
+
+    def find_plan_type_by_name(item)
+        PlanType.where(:name => item)
     end
 end
