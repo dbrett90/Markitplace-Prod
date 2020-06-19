2df14e156366e9eb54424ae87e5b2b144379b062
dbrett90 <brettd@bc.edu>
2020-06-19 16:07:20 -0400

    Updated the user creation font sze

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :destory]
+  #Need to make sure they are the only user to see or update profile.
   before_action :correct_user,   only: [:show, :edit, :update]
   #Only admin has capability to destroy users
   before_action :admin_user,     only: :destroy
