050925f3087f1f5297d0cc7504e8719f383ac12e
dbrett90 <brettd@bc.edu>
2020-06-22 16:04:15 -0400

    Update the quantity

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -28,7 +28,7 @@ class CartsController < ApplicationController
     def add_to_cart
         item = params[:one_off_product]
         one_off = find_one_off_by_name(item)
-        flash[:danger] = params[:quantity]
+        #flash[:danger] = params[:quantity]
         if cart_not_created?
             # empty_cart = Cart.create(products: [])
             testCart = Cart.new()
