70c54905967550b97bcf4651b2404e7fd597bd20
dbrett90 <brettd@bc.edu>
2020-06-24 14:09:25 -0400

    Update the carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -121,6 +121,187 @@ class CartsController < ApplicationController
         @total_price = sum_line_item_price(current_user.cart.line_items)
     end
 
+    def post_complete_checkout
+        #Make sure we change this to production when the time comes
+        Stripe.api_key = Rails.application.credentials.production[:stripe_api_key]
+        @cart_items = current_user.cart.line_items
+        token = params[:stripeToken]
+
+        #Complete Checkout for Subscriptions
+        @cart_items.each do |item|
+            if item.product_type != "One Off Product"
+                #Declare Variables up front
+                plan_type = find_plan_type_by_id(item.product_id)
+                fee_value = dyanmic_app_fee(plan_type)
+                plan_id = plan_type.plan_type_id
+                plan = Stripe::Plan.retrieve(plan_id, {stripe_account: plan_type.stripe_id})
+                connected_acct = plan_type.stripe_id
+                #Is Zip Code Ok? If so proceed
+                zipcode_val = params[:payment_shipping][:zipcode]
+                zipcode_val = zipcode_val.to_s
+                zipcode_list = parse_zipcodes(plan_type)
+                if limit_zipcodes(zipcode_val, zipcode_list)
+                    customer = if current_user.stripe_id[connected_acct].present?
+                        Stripe::Customer.retrieve(current_user.stripe_id[connected_acct], {stripe_account: plan_type.stripe_id})
+                        # flash[:danger] = "User already has a stripe ID!"
+                    else
+                        #Create customer in connected accounts environment.
+                        Stripe::Customer.create({
+                            email: current_user.email, 
+                            source:token,
+                        },
+                        {
+                            stripe_account: plan_type.stripe_id,
+                        })
+                        # Stripe::Customer.create(description: 'Test Customer')
+                        #Save the stripe id to the database
+                    end
+                    current_user.stripe_id[connected_acct] = customer.id
+                    options = {
+                        subscribed: true
+                    }
+                    current_user.plan_subscription_library_additions << plan_type
+
+                    #Doing a merge on cards
+                    options.merge!(
+                    card_last4: params[:user][:card_last4],
+                    card_exp_month: params[:user][:card_exp_month],
+                    card_exp_year: params[:user][:card_exp_year],
+                    card_type: params[:user][:card_brand]
+                    ) if params[:user][:card_last4]
+
+                    #Create the subscription
+                    subscription = Stripe::Subscription.create({
+                        customer: customer,
+                        items: [
+                            {
+                                plan: plan_id
+                            }
+                        ],
+                        application_fee_percent: fee_value,
+                        # application_fee: 0.50,
+                    }, stripe_account: plan_type.stripe_id)
+
+                    #Update the user hash
+                    current_user.stripe_subscription_id[plan.nickname.downcase] = subscription.id
+                    current_user.update(options)
+                    #For the hash portion
+                    current_user.save
+                else
+                    # stripped_name = strip_spaces(plan_type.name.downcase)
+                    symbolize = plan_type.name.downcase
+                    symbolize = symbolize.to_sym
+                    flash[:danger] = "Please note that for this subscription the zip code you provided is invalid. Delivery services are currently limited to " + plan_type.city_delivery + " for this product"
+                    redirect_to cart_path
+                    return
+                end
+            else 
+                item = find_one_off_by_id(item.product_id)
+                fee_amount = dyanmic_app_fee(item)
+                changed_price = item.price * 100
+                unless fee_amount == 0
+                    fee_amount = (changed_price / fee_amount).to_i
+                else
+                    fee_amount = 0
+                end
+                connected_acct = item.stripe_id
+                # flash[:warning] = item.price         
+
+                #Retrieve Customer
+                customer = if current_user.stripe_id[connected_acct].present?
+                    Stripe::Customer.retrieve(current_user.stripe_id[connected_acct], {stripe_account: item.stripe_id})
+                else
+                    #Create customer in connected accounts environment.
+                    Stripe::Customer.create({
+                        email: current_user.email, 
+                        source:token,
+                    },
+                    {
+                        stripe_account: item.stripe_id,
+                    })
+                    # Stripe::Customer.create(description: 'Test Customer')
+                    #Save the stripe id to the database
+                end
+                current_user.stripe_id[connected_acct] = customer.id
+
+                options = {
+                    subscribed: true
+                }
+        
+                options.merge!(
+                    card_last4: params[:user][:card_last4],
+                    card_exp_month: params[:user][:card_exp_month],
+                    card_exp_year: params[:user][:card_exp_year],
+                    card_type: params[:user][:card_brand]
+                    ) if params[:user][:card_last4]
+        
+                if charge_app_fee?(fee_amount)
+                    payment_intent = Stripe::PaymentIntent.create({
+                        payment_method_types: ['card'],
+                        amount: (item.price*100).to_i,
+                        currency: 'usd',
+                        application_fee_amount: fee_amount,
+                        capture_method: 'automatic',
+                        confirmation_method: 'automatic',
+                        # customer: customer,
+                        transfer_data: {
+                            destination: item.stripe_id,
+                        },
+                    })
+                else
+                    payment_intent = Stripe::PaymentIntent.create({
+                        payment_method_types: ['card'],
+                        amount: (item.price*100).to_i,
+                        currency: 'usd',
+                        capture_method: 'automatic',
+                        confirmation_method: 'automatic',
+                        # customer: customer,
+                        transfer_data: {
+                            destination: item.stripe_id,
+                        },
+                    })
+                end
+                card_brand = (params[:user][:card_brand]).downcase
+                payment_method_card = 'pm_card_' + card_brand
+                # flash[:danger] = payment_method_card
+                confirm_payment = Stripe::PaymentIntent.confirm(
+                    payment_intent.id,
+                    {payment_method: payment_method_card},
+                )
+                current_user.one_off_id[item.name.downcase] = payment_intent.id
+                current_user.update(options)
+                #For the hash portion
+                current_user.save
+            end
+            #Send out the confirmation emails
+            #Trigger Flash & The action mailers for confirmation
+            # OrderConfirmationMailer.customer_order_confirmation(current_user,
+            # params[:payment_shipping][:recipient_name], params[:payment_shipping][:street_address_1],
+            # params[:payment_shipping][:street_address_2], params[:payment_shipping][:city],
+            # params[:payment_shipping][:state], params[:payment_shipping][:zipcode]).deliver_now
+
+            # # #Hit the order confirmation and send over to the vendor(s)... Sends them a confirmation email about the order type. Can also view it in the stripe dashboard
+            # stripe_connect_users = StripeConnectUser.all
+            # sc_user_email_hash = find_sc_user_email(stripe_connect_users, current_user.cart.one_off_products, current_user.cart.plan_types)
+            # sc_user_email_hash.each do |vendor_email, product_array|
+            #     OrderConfirmationMailer.vendor_order_confirmation(current_user, params[:payment_shipping][:recipient_name], vendor_email, product_array, params[:payment_shipping][:street_address_1],
+            #     params[:payment_shipping][:street_address_2], params[:payment_shipping][:city],
+            #     params[:payment_shipping][:state], params[:payment_shipping][:zipcode]).deliver_now
+            # end 
+            # OrderConfirmationMailer.vendor_confirmation(current_user, sc_user_email, plan_type, params[:payment_shipping][:recipient_name], params[:payment_shipping][:street_address_1],
+            #     params[:payment_shipping][:street_address_2], params[:payment_shipping][:city],
+            #     params[:payment_shipping][:state], params[:payment_shipping][:zipcode]).deliver_now
+
+
+            #Confirm that the orders were made and notify customers on webpage
+            flash[:success] = "Thank you for your Purchase! You will receive
+            an email with a confirmation notice shortly."
+            current_user.cart.line_items.delete_all
+            redirect_to root_path
+        end
+            
+    end
+
     def add_to_cart_subscription
         item = params[:plan_type]
         plan_type = find_plan_type_by_name(item)
