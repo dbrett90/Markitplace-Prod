043fa7b9dc91c85a42d63d88edee8d198315baa7
dbrett90 <brettd@bc.edu>
2020-06-24 19:21:23 -0400

    Upadte the production

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -476,7 +476,7 @@ class CartsController < ApplicationController
             end
 
             card_brand = (params[:user][:card_brand]).downcase
-            payment_method_card = 'pm_card_' + card_brand
+            payment_method_card = 'card_' + card_brand
             # flash[:danger] = payment_method_card
             confirm_payment = Stripe::PaymentIntent.confirm(
                 payment_intent.id,
