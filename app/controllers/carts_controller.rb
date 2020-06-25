ec9430904dbea733576eb87563c2a9bff53690de
dbrett90 <brettd@bc.edu>
2020-06-25 17:01:35 -0400

    Update the paymne tintent

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -510,6 +510,7 @@ class CartsController < ApplicationController
             confirm_payment = Stripe::PaymentIntent.confirm(
                 payment_intent.id,
                 {payment_method: payment_method},
+                {stripe_account: item.stripe_id}
             )
     
             ##NEED TO CONFIRM THE PAYMENT AFTER THE FACT! CHECK THE DOCS FOR THIS
