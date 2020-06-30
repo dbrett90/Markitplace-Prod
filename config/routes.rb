0cc471b127b5a19ebee5c1a4cd3dc9591d31d4d8
dbrett90 <brettd@bc.edu>
2020-06-30 12:29:06 -0400

    Update the site

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/test-index', to:'carts#post_index'
   get '/test-checkout', to: 'carts#post_checkout'
   post '/test-checkout', to: 'carts#post_complete_checkout'
-  get '/mailing-list', to: 'static_pages#beta_test_list'
+  get '/beta-mailing-list', to: 'static_pages#beta_test_list'
   get '/home-signup-successful', to: 'stripe_connect_user#new'
   get '/our-team', to: 'static_pages#our_team'
   get '/add-to-cart', to: 'carts#add_to_cart'
