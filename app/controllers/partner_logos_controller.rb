f8d9e20842741fb5394387a9c17df14d34c64385
dbrett90 <brettd@bc.edu>
2020-07-13 12:21:41 -0400

    UPdated the playbook

--- app/controllers/partner_logos_controller.rb
+++ app/controllers/partner_logos_controller.rb
@@ -57,6 +57,8 @@ class PartnerLogosController < ApplicationController
         @playbook_user.phone_number = params[:playbook][:phone_number]
         @playbook_user.save         
         send_file "#{Rails.root}/app/assets/images/one_pager.pdf", type: "application/pdf", x_sendfile: true
+        redirect_to mealkit_playbook_path
+        flash[:success] = "Please check your downloads folder for the playbook."
     end
 
     private
