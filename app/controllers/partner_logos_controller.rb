a8ffe2a93459b936c861bbef62a0c6fe6ee0c4bb
dbrett90 <brettd@bc.edu>
2020-07-13 21:33:40 -0400

    Update the controller

--- app/controllers/partner_logos_controller.rb
+++ app/controllers/partner_logos_controller.rb
@@ -58,7 +58,7 @@ class PartnerLogosController < ApplicationController
         @playbook_user.save  
         #Download File & redirect. Flash notice that download complete./home/daniel/markitplace-prod/app/assets/images/markitplace_meal_kit_playbook.pdf
         #Why isn't download here working... upload to S3? 
-        data = open("https://markitplace-product-images.s3.us-east-2.amazonaws.com/Markitplace_Meal_Kit_Playbook.pdf")
+        data = open("https://markitplace-sitemaps.s3.us-east-2.amazonaws.com/Markitplace_Meal_Kit_Playbook.pdf")
         send_data data.read, filename: "Markitplace_Meal_Kit_Playbook.pdf", type: "application/pdf", disposition: 'inline', stream: 'true', buffer_size: '4096'
         # send_data "#{Rails.root}/app/assets/images/markitplace_meal_kit_playbook.pdf", type: "application/pdf", x_sendfile: true
         redirect_to mealkit_playbook_path
