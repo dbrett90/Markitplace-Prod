f08762fe6a391508d74c4cded97ef61c07bd7ca9
dbrett90 <brettd@bc.edu>
2020-07-22 22:19:06 -0400

    Update the action mailer asset host

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+#Set it for action mailer?
+config.action_mailer.asset_host = "https://www.markitplace.io"
 
 module Markitplace
   class Application < Rails::Application
